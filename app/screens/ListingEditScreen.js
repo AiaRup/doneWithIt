c30f20d119dda47079de00061e6815aa9551730f
Aia Rupsom <aiale88@gmail.com>
2020-06-23 10:08:21 +0300

    Create a custom hook for get user location

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
-import * as Location from 'expo-location';
 
 import {
   AppForm,
@@ -12,6 +11,7 @@ import {
   Screen,
   FormImagePicker,
 } from '../components';
+import useLocation from '../hooks/useLocation';
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label('Title'),
@@ -79,19 +79,7 @@ const categories = [
 ];
 
 export const ListingEditScreen = () => {
-  const [location, setLocation] = useState();
-
-  const getLoaction = async () => {
-    const { granted } = await Location.requestPermissionsAsync();
-    if (!granted) return;
-    const {
-      coords: { latitude, longitude },
-    } = await Location.getLastKnownPositionAsync();
-    setLocation({ latitude, longitude });
-  };
-  useEffect(() => {
-    getLoaction();
-  }, []);
+  const location = useLocation();
   return (
     <Screen style={styles.container}>
       <AppForm
@@ -102,7 +90,7 @@ export const ListingEditScreen = () => {
           category: null,
           images: [],
         }}
-        onSubmit={(values) => console.log(location)}
+        onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
         <FormImagePicker name="images" />
