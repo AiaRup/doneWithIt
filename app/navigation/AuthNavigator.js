04b4fb993c81ec3f164fa7fbf2c8f3cd161b6276
Aia Rupsom <aiale88@gmail.com>
2020-06-25 09:49:04 +0300

    add Auth navigation stack

---
+++ app/navigation/AuthNavigator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { WelcomeScreen, LoginScreen, RegisterScreen } from '../screens';
+
+const Stack = createStackNavigator();
+
+export default AuthNavigator = () => {
+  <Stack.Navigator>
+    <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
+  </Stack.Navigator>;
+};
