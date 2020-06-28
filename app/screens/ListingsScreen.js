6330147fcceda87fcb738a210275ff2bd0d29dd7
Aia Rupsom <aiale88@gmail.com>
2020-06-28 20:17:55 +0300

    Handling server error on the front

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 
-import { Screen, Card } from '../components';
+import { Screen, Card, AppText, AppButton } from '../components';
 import colors from '../config/colors';
 import routes from '../navigation/routes';
 import listingsApi from '../api/listings';
 
 export const ListingsScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadListings();
@@ -15,10 +16,19 @@ export const ListingsScreen = ({ navigation }) => {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setError(true);
+
+    setError(false);
     setListings(response.data);
   };
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrive the listings.</AppText>
+          <AppButton title="Retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
