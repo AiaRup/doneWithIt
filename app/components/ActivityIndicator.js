bd1a2f0e96486e3844cf4f1eaa241afa14a78d64
Aia Rupsom <aiale88@gmail.com>
2020-06-29 13:30:24 +0300

    add Activity indicator component

---
+++ app/components/ActivityIndicator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LottieView from 'lottie-react-native';
+
+export const ActivityIndicator = ({ visible = false }) => {
+  if (!visible) return null;
+
+  return (
+    <LottieView
+      autoPlay
+      loop
+      source={require('../assets/animations/loader.json')}
+    />
+  );
+};
