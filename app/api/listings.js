ac7d2599674d584f942e975a840c4fbab5e09687
Aia Rupsom <aiale88@gmail.com>
2020-06-30 18:52:53 +0300

    extend api layer to send a listing post to the server

--- app/api/listings.js
+++ app/api/listings.js
@@ -4,6 +4,33 @@ const endpoint = '/listings';
 
 const getListings = () => client.get(endpoint);
 
+const addListing = ({
+  title,
+  price,
+  category,
+  description,
+  images,
+  location,
+}) => {
+  const data = new FormData();
+  data.append('title', title);
+  data.append('price', price);
+  data.append('categoryId', category.value);
+  data.append('description', description);
+
+  images.forEach((image, index) =>
+    data.append('images', {
+      name: 'image' + index,
+      type: 'image/jpeg',
+      uri: image,
+    })
+  );
+
+  location && data.append('location', JSON.stringify(location));
+  return client.post(endpoint, data);
+};
+
 export default {
+  addListing,
   getListings,
 };
