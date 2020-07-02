6c3d9e33b9fccad2cac17469c11236e9daea1a5b
Aia Rupsom <aiale88@gmail.com>
2020-07-02 20:21:27 +0300

    check internet conection on App component

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
 
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
+import { View } from 'react-native';
 
 export default function App() {
-  return (
+  const netInfo = useNetInfo();
+
+  return netInfo.isInternetReachable ? (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
     </NavigationContainer>
+  ) : (
+    <View></View>
   );
 }
