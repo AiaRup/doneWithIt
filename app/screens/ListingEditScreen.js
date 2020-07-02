f1d034f38d8a5484dbc7226bab747133ca5561d4
Aia Rupsom <aiale88@gmail.com>
2020-07-02 11:01:11 +0300

    reset progress between uploads

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -86,6 +86,7 @@ export const ListingEditScreen = () => {
   const [progress, setProgress] = useState(0);
 
   const handleSubmit = async (listing) => {
+    setProgress(0);
     setUploadVisible(true);
     const result = await listingsApi.addListing(
       { ...listing, location },
