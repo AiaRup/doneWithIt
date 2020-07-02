2d550d837065664ea0f802c30688f3e4561364c8
Aia Rupsom <aiale88@gmail.com>
2020-07-02 10:26:17 +0300

    create Upload screen with a text progress

---
+++ app/screens/UploadScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View, Modal } from 'react-native';
+import { AppText } from '../components';
+
+export const UploadScreen = ({ progress = 0, visible = false }) => {
+  return (
+    <Modal visible={visible}>
+      <View style={styles.container}>
+        <AppText>{progress * 100}%</AppText>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
