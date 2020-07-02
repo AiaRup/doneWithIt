e89ddd4718ae69be469fe65c9387491920331331
Aia Rupsom <aiale88@gmail.com>
2020-07-02 10:34:15 +0300

    call Upload screen when adding a new listing

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Modal } from 'react-native';
+
 import { AppText } from '../components';
 
 export const UploadScreen = ({ progress = 0, visible = false }) => {
