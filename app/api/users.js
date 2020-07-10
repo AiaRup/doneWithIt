ac725a14341dda196f00107394f2cb25a9d4b82c
Aia Rupsom <aiale88@gmail.com>
2020-07-10 19:06:47 +0300

    add register functionality

---
+++ app/api/users.js
@@ -0,0 +1,7 @@
+import client from './client';
+
+const register = (userInfo) => client.post('/users', userInfo);
+
+export default {
+  register,
+};
--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Yup from 'yup';
 import { StyleSheet } from 'react-native';
 
-import { Screen, AppForm, AppFormField, SubmitButton } from '../components';
+import {
+  Screen,
+  AppForm,
+  AppFormField,
+  SubmitButton,
+  ErrorMessage,
+} from '../components';
+import userApi from '../api/users';
+import useAuth from '../auth/useAuth';
+import authApi from '../api/auth';
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label('Name'),
@@ -11,13 +20,36 @@ const validationSchema = Yup.object().shape({
 });
 
 export const RegisterScreen = () => {
+  const auth = useAuth();
+  const [error, setError] = useState();
+
+  const handleSubmit = async (userInfo) => {
+    const result = await userApi.register(userInfo);
+
+    if (!result.ok) {
+      if (result.data) setError(result.data.error);
+      else {
+        setError('An unexpected error occurred.');
+        console.log(result);
+      }
+      return;
+    }
+
+    const { data: authToken } = await authApi.login(
+      userInfo.email,
+      userInfo.password
+    );
+    auth.logIn(authToken);
+  };
+
   return (
     <Screen style={styles.container}>
       <AppForm
         initialValues={{ name: '', email: '', password: '' }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
+        <ErrorMessage error={error} visible={error} />
         <AppFormField
           autoCorrect={false}
           icon="account"
