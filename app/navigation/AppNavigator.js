5bb92bc020299bfa74b50670b70fdc30a17d23c5
Aia Rupsom <aiale88@gmail.com>
2020-07-11 11:05:30 +0300

    get permission for push notifications

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 import { ListingEditScreen } from '../screens';
 import FeedNavigator from './FeedNavigator';
@@ -10,36 +12,52 @@ import routes from './routes';
 
 const Tab = createBottomTabNavigator();
 
-export default AppNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen
-      name={routes.FEED}
-      component={FeedNavigator}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="home" color={color} size={size} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name={routes.LISTINGS_EDIT}
-      component={ListingEditScreen}
-      options={({ navigation }) => ({
-        tabBarButton: () => (
-          <NewListingButton
-            onPress={() => navigation.navigate(routes.LISTINGS_EDIT)}
-          />
-        ),
-      })}
-    />
-    <Tab.Screen
-      name={routes.ACCOUNT}
-      component={AccountNavigator}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
-        ),
-      }}
-    />
-  </Tab.Navigator>
-);
+export default AppNavigator = () => {
+  useEffect(() => {
+    registerForPushNotifications();
+  }, []);
+
+  const registerForPushNotifications = async () => {
+    try {
+      const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (!permission.granted) return;
+      const token = Notifications.getExpoPushTokenAsync();
+      console.log(token);
+    } catch (error) {
+      console.log('Error getting a push token', error);
+    }
+  };
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name={routes.FEED}
+        component={FeedNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name={routes.LISTINGS_EDIT}
+        component={ListingEditScreen}
+        options={({ navigation }) => ({
+          tabBarButton: () => (
+            <NewListingButton
+              onPress={() => navigation.navigate(routes.LISTINGS_EDIT)}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name={routes.ACCOUNT}
+        component={AccountNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
